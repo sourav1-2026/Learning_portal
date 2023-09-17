918104decdaee1a0f41ad49f9439e9347bcc291f
sourav1-2026 <souravkshaw2019@gmail.com>
2023-09-17 11:20:48 -0700

    first commit

---
+++ Screen/Dashboard/MainLayout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MainLayout() {
+  return (
+    <View>
+      <Text>MainLayout</Text>
+    </View>
+  )
+}
