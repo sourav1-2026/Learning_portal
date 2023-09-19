22a3ba698dbe2d2512b06182e14b51a40e1e8571
sourav1-2026 <souravkshaw2019@gmail.com>
2023-09-18 23:52:57 -0700

    different method of tab

--- Components/TabBar.tsx
+++ Components/TabBar.tsx
@@ -1,41 +1,91 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+  BottomTabBar,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 
 import Home from '../Screen/Dashboard/Home';
 import Profile from '../Screen/Dashboard/Profile';
 import Search from '../Screen/Dashboard/Search';
 
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import LottieView from 'lottie-react-native';
+
 export default function TabBar() {
   const Tab = createBottomTabNavigator();
   return (
+    // <Tab.Navigator
+    //   screenOptions={{
+    //     tabBarActiveTintColor: '#e91e63',
+    //     tabBarActiveBackgroundColor: '#F3F3F3',
+    //     tabBarLabelStyle: {
+    //       fontSize: 12,
+    //       fontWeight: 'bold',
+    //       color: 'blue',
+    //     },
+    //   }}>
+    //   <Tab.Screen
+    //     name="Home"
+    //     component={Home}
+    //     options={{
+    //       tabBarLabel: 'Home',
+    //       tabBarIcon: ({color, size}) => (
+    //         <Icon name="home" color={color} size={28} />
+    //       ),
+    //     }}
+    //   />
+    //   <Tab.Screen name="Profile" component={Profile} />
+    //   <Tab.Screen name="Search" component={Search} />
+    // </Tab.Navigator>
+
+    // options -2:
+    // --------------------
+
     <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: '#e91e63',
-        // tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.1)',
-        // headerShown: false,
-        tabBarActiveBackgroundColor: '#F3F3F3',
-      }}>
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          // title: 'Home3',
-          tabBarLabel: 'Home',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="home" color={color} size={28} />
-          ),
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: 'bold',
-            color: 'blue',
-          },
-          // tabBarAccessibilityLabel: 'Navigate to Home',
-        }}
-      />
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Profile') {
+            iconName = 'profile';
+          } else {
+            iconName = 'search1';
+          }
+          return <AntDesign name={iconName} size={30} color="#900" />;
+        },
+        tabBarActiveTintColor: 'green',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Search" component={Search} />
     </Tab.Navigator>
+
+    // <Tab.Navigator
+    //   screenOptions={({route}) => ({
+    //     tabBarIcon: ({focused, color, size}) => {
+    //       let filePath;
+    //       if (route.name === 'Home') {
+    //         filePath = require('../Lottie/home.json');
+    //       } else if (route.name === 'Profile') {
+    //         filePath = require('../Lottie/Profile.json');
+    //       } else {
+    //         filePath = require('../Lottie/search.json');
+    //       }
+    //       <LottieView source={filePath} autoPlay={focused} />;
+    //     },
+    //     tabBarActiveTintColor: 'green',
+    //     tabBarInactiveTintColor: 'gray',
+    //   })}>
+    //   <Tab.Screen name="Home" component={Home} />
+    //   <Tab.Screen name="Profile" component={Profile} />
+    //   <Tab.Screen name="Search" component={Search} />
+    // </Tab.Navigator>
+
+    // option -3:
+    // ----------------
+    // https://www.youtube.com/watch?v=8gTaXwmnuNE
   );
 }
